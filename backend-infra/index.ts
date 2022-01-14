d9426dae9ce5b9f544327078a3caf55c57fc73d9
Alex Mullans <alexmullans@outlook.com>
2022-01-14 16:59:01 -0500

    Azure CR and Docker container publish

--- backend-infra/index.ts
+++ backend-infra/index.ts
@@ -3,8 +3,36 @@ import * as containerregistry from "@pulumi/azure-native/containerregistry";
 import * as resources from "@pulumi/azure-native/resources";
 import * as web from "@pulumi/azure-native/web";
 
-// Create an Azure Resource Group
+import * as docker from "@pulumi/docker";
+
 const resourceGroup = new resources.ResourceGroup("deliverate-backend");
+
+const registry = new containerregistry.Registry("registry", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: "Basic",
+    },
+    adminUserEnabled: true,
+});
+
+const credentials = containerregistry.listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+});
+
+const adminUsername = credentials.apply(credentials => credentials.username!);
+const adminPassword = credentials.apply(credentials => credentials.passwords![0].value!);
+
+const imageName = "deliverate-backend";
+const image = new docker.Image(imageName, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:latest`,
+    build: {context: `../backend/`},
+    registry: {
+        server: registry.loginServer,
+        username: adminUsername,
+        password: adminPassword,
+    },
+});
 
 const plan = new web.AppServicePlan("deliverate-plan", {
     resourceGroupName: resourceGroup.name,
